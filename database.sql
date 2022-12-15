f97817855e702e99b82ce43e9cb8c1cf2e2e33f2
john <john.flouis1@gmail.com>
2022-12-15 21:35:35 +0100

    finish 2.1 Basic structure of the database
    organise the code

--- database.sql
+++ database.sql
@@ -1,13 +1,13 @@
-﻿CREATE PROCEDURE createAllTables
+﻿--> Guide to quick searching through the project: search for "--> point number in the milestone description"
+--> 2.1a createAllTables
+CREATE PROCEDURE createAllTables
 AS
-
 -- USERS
 CREATE TABLE systemUser (
 	username VARCHAR(20),
 	password VARCHAR(20),
 	PRIMARY KEY(username)
 );
-
 CREATE TABLE fan (
 	national_id INT,
 	name VARCHAR(20),
@@ -19,7 +19,6 @@ CREATE TABLE fan (
 	PRIMARY KEY (nationalId),
 	FOREIGN KEY (username) REFERENCES systemUser
 );
-
 CREATE TABLE stadiumManager (
 	id INT IDENTITY,
 	name VARCHAR(20),
@@ -28,8 +27,7 @@ CREATE TABLE stadiumManager (
 	PRIMARY KEY (id),
 	FOREIGN KEY (username) REFERENCES systemUser,
 	FOREIGN KEY (stadium_id) REFERENCES stadium
-)
-
+);
 CREATE TABLE clubRepresentative (
 	id INT IDENTITY,
 	name VARCHAR(20),
@@ -38,23 +36,21 @@ CREATE TABLE clubRepresentative (
 	PRIMARY KEY (id),
 	FOREIGN KEY	(username) REFERENCES systemUser,
 	FOREIGN KEY	(club_id) REFERENCES club
-)
-
+);
 CREATE TABLE sportsAssociationManager (
 	id INT IDENTITY,
 	name VARCHAR(20),
 	username VARCHAR(20),
 	PRIMARY KEY (id),
 	FOREIGN KEY (username) REFERENCES systemUser
-)
-
+);
 CREATE TABLE systemAdmin (
 	id INT IDENTITY,
 	name VARCHAR(20),
 	username VARCHAR(20),
 	PRIMARY KEY (id),
 	FOREIGN KEY (username) REFERENCES systemUser
-)
+);
 
 -- OTHER ENTITIES
 CREATE TABLE stadium (
@@ -64,23 +60,20 @@ CREATE TABLE stadium (
 	capacity INT,
 	status BIT,
 	PRIMARY KEY (id)
-)
-
+);
 CREATE TABLE club (
 	id INT IDENTITY,
 	name VARCHAR(20),
 	location VARCHAR(20),
 	PRIMARY KEY (id)
-)
-
+);
 CREATE TABLE ticket (
 	id INT IDENTITY,
 	status BIT,
 	match_id INT,
 	PRIMARY KEY (id),
 	FOREIGN KEY (match_id) REFERENCES match
-)
-
+);
 CREATE TABLE match (
 	id INT IDENTITY,
 	startTime DATETIME,
@@ -92,7 +85,7 @@ CREATE TABLE match (
 	FOREIGN KEY (hostClub_id) REFERENCES club,
 	FOREIGN KEY (guestClub_id) REFERENCES club,
 	FOREIGN KEY (stadium_id) REFERENCES stadium 
-)
+);
 
 -- RELATIONS
 CREATE TABLE ticketBuyingTransaction (
@@ -102,8 +95,7 @@ CREATE TABLE ticketBuyingTransaction (
 	capacity INT,
 	status BIT,
 	PRIMARY KEY (id)
-)
-
+);
 CREATE TABLE hostRequest (
 	id INT IDENTITY,
 	name VARCHAR(20),
@@ -111,9 +103,10 @@ CREATE TABLE hostRequest (
 	capacity INT,
 	status BIT,
 	PRIMARY KEY (id)
-)
+);
 GO;
 
+--> 2.1b dropAllTables
 CREATE PROCEDURE dropAllTables
 AS
 DROP TABLE systemUser;
@@ -130,4 +123,31 @@ DROP TABLE ticketBuyingTransaction;
 DROP TABLE hostRequest;
 GO;
 
+--> 2.1c dropAllProceduresFunctionsViews
+CREATE PROCEDURE dropAllProceduresFunctionsViews
+AS
+-- TODO
+GO;
+
+--> 2.1d clearAllTables
+CREATE PROCEDURE clearAllTables
+AS
+TRUNCATE TABLE systemUser;
+TRUNCATE TABLE fan;
+TRUNCATE TABLE stadiumManager;
+TRUNCATE TABLE clubRepresentative;
+TRUNCATE TABLE sportsAssociationManager;
+TRUNCATE TABLE systemAdmin;
+TRUNCATE TABLE stadium;
+TRUNCATE TABLE club;
+TRUNCATE TABLE ticket;
+TRUNCATE TABLE match;
+TRUNCATE TABLE ticketBuyingTransaction;
+TRUNCATE TABLE hostRequest;
+GO;
+
+
 EXEC createAllTables;
+EXEC dropAllTables;
+EXEC dropAllProcedureFunctionsViews;
+EXEC clearAllTables;
