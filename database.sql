38656b821c349003bdac73cf38b4f3a039ac2210
john <john.flouis1@gmail.com>
2022-12-15 20:56:28 +0100

    create first three tables

---
+++ database.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE createAllTables
+AS
+
+CREATE TABLE systemUser (
+	username VARCHAR(20),
+	password VARCHAR(20),
+	PRIMARY KEY(username)
+);
+
+CREATE TABLE fan (
+	national_id INT,
+	name VARCHAR(20),
+	birthDate DATE,
+	address VARCHAR(20),
+	phoneNumber INT,
+	status BIT,
+	username VARCHAR(20),
+	PRIMARY KEY (nationalId),
+	FOREIGN KEY (username) REFERENCES systemUser
+);
+
+CREATE TABLE stadiumManager (
+	id INT IDENTITY,
+	name VARCHAR(20),
+	stadium_id INT,
+	username VARCHAR(20),
+	PRIMARY KEY (id),
+	FOREIGN KEY (username) REFERENCES systemUser,
+	FOREIGN KEY (stadium_id) REFERENCES stadium
+)
+
+GO;
+
+
+EXEC createAllTables;
