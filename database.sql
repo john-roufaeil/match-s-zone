43997fa1c3d6d19bff4d6b300d4a6c583cb81896
john <john.flouis1@gmail.com>
2022-12-15 22:28:38 +0100

    finish 2.2 basic data retrieval
    fix hostRequest table

--- database.sql
+++ database.sql
@@ -102,11 +102,14 @@ CREATE TABLE ticketBuyingTransaction (
 );
 CREATE TABLE hostRequest (
 	id INT IDENTITY,
-	name VARCHAR(20),
-	location VARCHAR(20),
-	capacity INT,
+	representative_id INT,
+	manager_id INT,
+	match_id INT,
 	status BIT,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (representative_id) REFERENCES clubRepresentative,
+	FOREIGN KEY (manager_id) REFERENCES stadiumManager,
+	FOREIGN KEY (match_id) REFERENCES match
 );
 GO;
 
@@ -160,19 +163,76 @@ GO;
 
 --> 2.2a allAssocManagers
 CREATE VIEW allAssocManagers AS
-SELECT username, password, name
-FROM sportsAssociationManager;
+SELECT SPA.username, SU.password, SPA.name
+FROM sportsAssociationManager SPA 
+INNER JOIN systemUser SU ON R.username = SPA.username;
 GO;
+
 --> 2.2b allClubRepresentatives
 CREATE VIEW allClubRepresentatives AS
-SELECT username,password, name, 
+SELECT CR.username, SU.password, CR.name, CR.club_id
+FROM clubRepresentative CR
+INNER JOIN systemUser SU ON CR.username = SU.username;
+GO;
+
+--> 2.2c allStadiumManagers
+CREATE VIEW allStadiumManagers AS
+SELECT SM.username, SU.password, SM.name, SM.stadium_id
+FROM stadiumManager SM
+INNER JOIN systemUser SU ON SM.username = SU.username;
+GO;
+
+--> 2.2d allFans
+CREATE VIEW allFans AS
+SELECT F.username, SU.password, F.name, F.national_id, F.birthDate, F.status
+FROM fan F
+INNER JOIN systemUser SU ON F.username = SU.username;
+GO;
+
+--> 2.2e allMatches  Fetches the name of the host club, the name of the guest club and the start time for all matches.
+CREATE VIEW allMatches AS
+SELECT C1.name hostClub, C2.name guestClub, M.startTime
+FROM match M
+INNER JOIN club C1 ON C1.id = M.hostClub_id
+INNER JOIN club C2 ON C2.id = M.guestClub_id;
+GO;
+
+--> 2.2f allTickets Fetches the name of the host club, the name of the guest club, the name of the stadium that will host the match and the start time of the match for all tickets.
+CREATE VIEW allTickets AS
+SELECT C1.name hostClub, C2.name guestClub, S.name stadium, M.startTime time
+FROM ticket T
+INNER JOIN match M	 ON T.match_id = M.id
+INNER JOIN club C1	 ON M.hostClub_id = C1.id
+INNER JOIN club C2	 ON M.guestClub_id = C2.id
+INNER JOIN stadium S ON M.stadium_id = S.id;
+GO;
+
+--> 2.2g allCLubs Fetches the name and location for all clubs.
+CREATE VIEW allClubs AS
+SELECT C.name, C.location
+FROM club C;
+GO;
+
+--> 2.2h allStadiums Fetches the name ,location, capacity and status (available or unavailable) for all stadiums.
+CREATE VIEW allStadiums AS
+SELECT S.name, S.location, S.capacity, S.status
+FROM stadium S;
+GO;
+
+--> 2.2i allRequests Fetches the username of the club representative sending the request, username of the stadium manager receiving the request and the status of the request for all requests.
+CREATE VIEW allRequests AS
+SELECT CR.username, SM.username, HR.status
+FROM hostRequest HR
+INNER JOIN clubRepresentative CR ON HR.representative_id = CR.id
+INNER JOIN stadiumManager SM ON HR.manager_id = SM.id;
+GO;
 
 
 
 														--|REFERENCE|--
 
 -- TABLES:	systemUser				(*username, password)
---			fan						(*nationa_id, name, birthDate, address, phoneNumber, status, .username)
+--			fan						(*national_id, name, birthDate, address, phoneNumber, status, .username)
 --			stadiumManager			(*id, name, .stadium_id, .username)
 --			clubRepresentative		(*id, name, .club_id, .username) 
 --			sportsAssociationManager(*id, name, .username)
@@ -182,4 +242,4 @@ SELECT username,password, name,
 --			ticket					(*id, status, .match_id)
 --			match					(*id, startTime, endTime, .hostClub_id, .guestClub_id, .stadium_id)
 --			ticketBuyingTransaction	(.fanNational_id, .ticket_id)
---			hostRequest				(*id, name, location, capacity, status)
+--			hostRequest				(*id, .representative_id, .manager_id, .match_id, status)
